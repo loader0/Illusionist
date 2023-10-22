4fc5ac95f82e5726540ac8a184a83322b9f9728b
Oscar Torres <loader0@hotmail.com>
2023-10-21 18:29:51 -0700

    Primer Commit Inicia proyecto

---
+++ src/main/java/com/sro/Illusionist/IllusionistApplication.java
@@ -0,0 +1,13 @@
+package com.sro.Illusionist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllusionistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IllusionistApplication.class, args);
+	}
+
+}
